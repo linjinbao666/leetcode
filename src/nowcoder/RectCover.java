72d69392f4abe67b96a49bcba27c031843c46cd3
linjinbao666 <linjinbao666@gamil.com>
2019-09-12 14:15:57 +0800

    2019年9月12日

---
+++ src/nowcoder/RectCover.java
@@ -0,0 +1,13 @@
+package nowcoder;
+
+/**
+ * 矩形覆盖
+ */
+public class RectCover {
+    public int rectCover(int target){
+        if (target==1) return 1;
+        if (target==2) return 1;
+        if (target==3) return 1;
+        return 0;
+    }
+}
