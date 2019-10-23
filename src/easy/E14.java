98ff28c32f584582274332deb8f84fabbfbb1b75
linjinbao666@gmail.com <linjinbao666@gmail.com>
2019-10-23 17:59:49 +0800

    update 2019-10-23

--- src/easy/E14.java
+++ src/easy/E14.java
@@ -5,14 +5,19 @@ package easy;
  */
 public class E14 {
     public String longestCommonPrefix(String[] strs) {
-        int len = 0;
-        for (int i = 0; i < strs.length; i++) {
-            len = Math.min(len, strs[i].length());
-        }
+        int len = strs.length;
         if (len==0) return "";
+        if (len==1) return strs[0];
 
+        String str = strs[0];
 
+        for (int i=1; i < strs.length; i++){
+            while (strs[i].indexOf(str)!=0){
+                str = str.substring(0,str.length()-1);
+                if (str=="") return "";
+            }
+        }
+        return str;
 
-        return null;
     }
 }
